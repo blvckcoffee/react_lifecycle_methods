99d70e0232be38782c13fc28f6663a4cd2c4fb00
blvckcoffee <joe.johnson.dev@gmail.com>
2021-02-12 08:41:38 -0600

    complete react lifecycle exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      text: "Here's some filler text.",
+    };
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleClick() {
+    this.setState({
+      hasLoaded: this.state.hasLoaded ? false : true,
+    });
+  }
+
+  componentDidMount() {
+    this.setState({
+      hasLoaded: true,
+    });
   }
 
   render() {
-    return (
-      <div>
-        <h1>Welcome to React!</h1>
-        <hr />
-      </div>
-    );
+    if (this.state.hasLoaded) {
+      return (
+        <div>
+          <h1>{this.state.text}</h1>
+          <hr />
+          <input
+            type='text'
+            placeholder={this.state.text}
+            onChange={this.handleInput}
+          />
+          <button onClick={this.handleClick}>Do Something</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Loading...</h1>
+          <button onClick={this.handleClick}>Do Something</button>
+        </div>
+      );
+    }
   }
 }
 
